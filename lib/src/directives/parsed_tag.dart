d3c6ac1a704c773a14230e0e4e2c45c2126c5726
Kelvin Kavisi <68240897+kekavc24@users.noreply.github.com>
2025-02-19 10:38:23 +0000

    refactor: format a parsed tag to verbatim on creation

--- lib/src/directives/parsed_tag.dart
+++ lib/src/directives/parsed_tag.dart
@@ -1,12 +1,30 @@
 part of 'directives.dart';
 
+String _formatAsVerbatim(SpecificTag tag, String suffix) {
+  var prepend = '';
+  var formattedSuffix = suffix.trim();
+
+  if (tag is GlobalTag) {
+    if (formattedSuffix.isEmpty) {
+      throw FormatException('A global tag must have a non-empty suffix');
+    }
+
+    prepend = tag.prefix;
+    prepend += prepend.endsWith('/') || prepend.endsWith(':') ? '' : ':';
+  } else {
+    prepend = tag.toString();
+  }
+
+  return _wrapAsVerbatim('$prepend$formattedSuffix');
+}
+
 final class ParsedTag<T> implements _ResolvedTag {
-  ParsedTag({required SpecificTag<T> resolvedTag, required this.suffix})
-    : _resolvedTag = resolvedTag;
+  ParsedTag(this._resolvedTag, String suffix)
+    : _verbatim = _formatAsVerbatim(_resolvedTag, suffix);
 
   final SpecificTag<T> _resolvedTag;
 
-  final String suffix;
+  final String _verbatim;
 
   @override
   String get prefix => _resolvedTag.prefix;
@@ -15,26 +33,15 @@ final class ParsedTag<T> implements _ResolvedTag {
   TagHandle get tagHandle => _resolvedTag.tagHandle;
 
   @override
-  String get verbatim {
-    var prepend = '';
-
-    if (_resolvedTag is GlobalTag) {
-      prepend = _resolvedTag.prefix;
-      prepend += prepend.endsWith('/') || prepend.endsWith(':') ? '' : ':';
-    } else {
-      prepend = _resolvedTag.toString();
-    }
-
-    return '!<$prepend$suffix>';
-  }
+  String get verbatim => _verbatim;
 
   @override
   bool operator ==(Object other) =>
-      other is ParsedTag && other.verbatim == verbatim;
+      other is ParsedTag && other._verbatim == _verbatim;
 
   @override
-  int get hashCode => verbatim.hashCode;
+  int get hashCode => _verbatim.hashCode;
 
   @override
-  String toString() => verbatim;
+  String toString() => _verbatim;
 }
