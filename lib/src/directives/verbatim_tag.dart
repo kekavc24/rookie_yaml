c6381e81243faa867dad4b37bb313e85ac4de0cb
Kelvin Kavisi <68240897+kekavc24@users.noreply.github.com>
2025-02-19 11:58:05 +0000

    feat: add verbatim tag parsing logic

--- lib/src/directives/verbatim_tag.dart
+++ lib/src/directives/verbatim_tag.dart
@@ -1,5 +1,9 @@
 part of 'directives.dart';
 
+const _onNonEmptyVerbationUri =
+    'Verbatim tags are never resolved and should '
+    'have a non-empty suffix';
+
 final _verbatimStart = GraphemeChar.wrap('<');
 final _verbatimEnd = GraphemeChar.wrap('>');
 
@@ -23,9 +27,7 @@ final class VerbatimTag implements _ResolvedTag {
         'Verbatim tags with a local tag must have a single "!" prefix',
       );
     } else if (uri.isEmpty) {
-      throw FormatException(
-        'Verbatim tags are never resolved and should have a non-empty suffix',
-      );
+      throw FormatException(_onNonEmptyVerbationUri);
     }
 
     return VerbatimTag._(_wrapAsVerbatim(uri));
@@ -49,4 +51,71 @@ final class VerbatimTag implements _ResolvedTag {
 
   @override
   int get hashCode => verbatim.hashCode;
+}
+
+VerbatimTag parseVerbatimTag(ChunkScanner scanner) {
+  var charAtCursor = scanner.charAtCursor;
+
+  void skipAndMove() {
+    scanner.skipCharAtCursor();
+    charAtCursor = scanner.charAtCursor;
+  }
+
+  final buffer = StringBuffer();
+
+  void isNotNullOrMatches({
+    required bool Function(ReadableChar char) matcher,
+    required String errorOnMismatch,
+  }) {
+    if (charAtCursor == null || !matcher(charAtCursor!)) {
+      throw FormatException(errorOnMismatch);
+    }
+
+    buffer.write(charAtCursor!.string);
+  }
+
+  // Must start with a leading "!"
+  isNotNullOrMatches(
+    matcher: (char) => char == _tagIndicator,
+    errorOnMismatch: 'A verbatim tag must start with "!"',
+  );
+  skipAndMove();
+
+  final GraphemeChar(string: vStart, unicode: vsCode) = _verbatimStart;
+
+  // Must be followed by an opening bracket "<"
+  isNotNullOrMatches(
+    matcher: (char) => char.unicode == vsCode,
+    errorOnMismatch: 'Expected to find a "$vStart" after "!"',
+  );
+  skipAndMove();
+
+  // This may be a local tag instead of a global one
+  if (charAtCursor == _tagIndicator) {
+    skipAndMove();
+  }
+
+  // We can safely extract the remaining as uri characters
+  final uri = _parseTagUri(
+    scanner,
+    allowRestrictedIndicators: true,
+    isVerbatim: true,
+  );
+
+  if (uri.isEmpty) {
+    throw FormatException(_onNonEmptyVerbationUri);
+  }
+
+  charAtCursor = scanner.charAtCursor;
+  buffer.write(uri);
+
+  final GraphemeChar(string: vEnd, unicode: veCode) = _verbatimEnd;
+
+  isNotNullOrMatches(
+    matcher: (char) => char.unicode == veCode,
+    errorOnMismatch: 'Expected to find a "$vEnd" after "!"',
+  );
+  skipAndMove();
+
+  return VerbatimTag._(buffer.toString());
 }
