1c55ee0e4e637237a2355068ab8de7ebcdfd9397
Kelvin Kavisi <68240897+kekavc24@users.noreply.github.com>
2025-03-25 06:09:01 +0000

    feat: add a tag that maps a node to a custom type

---
+++ lib/src/directives/resolver_tag.dart
@@ -0,0 +1,28 @@
+part of 'directives.dart';
+
+/// A tag that wraps a [ResolvedTag] and defines a mapping function for any
+/// node annotated with wrapped tag.
+final class NativeResolverTag<O> implements ResolvedTag {
+  NativeResolverTag(this._resolvedTag, {required this.resolver});
+
+  /// Underlying [ResolvedTag]
+  final ResolvedTag _resolvedTag;
+
+  /// Function that generates
+  final O Function(Node node) resolver;
+
+  @override
+  String get prefix => _resolvedTag.prefix;
+
+  @override
+  TagHandle get tagHandle => _resolvedTag.tagHandle;
+
+  @override
+  String get verbatim => _resolvedTag.verbatim;
+
+  @override
+  bool operator ==(Object other) => _resolvedTag == other;
+
+  @override
+  int get hashCode => _resolvedTag.hashCode;
+}
