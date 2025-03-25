6d89871af5a032182bb3ace86beb047180fe1069
Kelvin Kavisi <68240897+kekavc24@users.noreply.github.com>
2025-03-25 06:10:22 +0000

    feat: add YAML tags for the failsafe schema

---
+++ lib/src/parser/yaml_failsafe_schema.dart
@@ -0,0 +1,42 @@
+part of 'yaml_parser.dart';
+
+/// Default `YAML` uri prefix
+const _yamlPrefix = 'tag:yaml.org,2002:';
+
+/// Default handle for the global `YAML` tag.
+///
+/// See [_yamlPrefix]
+final _defaultYamlHandle = TagHandle.secondary();
+
+/// `YAML` global tag
+///
+/// ```text
+/// %TAG !! tag:yaml.org,2002:
+/// ```
+final _yamlGlobalTag = GlobalTag.fromTagUri(_defaultYamlHandle, _yamlPrefix);
+
+/// Generic mapping
+final mappingTag = LocalTag.fromTagUri(_defaultYamlHandle, 'map');
+
+/// Generic sequence
+final sequenceTag = LocalTag.fromTagUri(_defaultYamlHandle, 'seq');
+
+/// Generic string
+final stringTag = LocalTag.fromTagUri(_defaultYamlHandle, 'str');
+
+//
+// ** JSON SCHEMA TAGS **
+// This schema is supported by YAML out of the box.
+//
+
+/// `JSON` null
+final nullTag = LocalTag.fromTagUri(_defaultYamlHandle, 'null');
+
+/// `JSON` boolean
+final booleanTag = LocalTag.fromTagUri(_defaultYamlHandle, 'bool');
+
+/// `JSON` integer
+final integerTag = LocalTag.fromTagUri(_defaultYamlHandle, 'int');
+
+/// `JSON` floating point
+final floatTag = LocalTag.fromTagUri(_defaultYamlHandle, 'float');
---
+++ lib/src/parser/yaml_parser.dart
@@ -0,0 +1,5 @@
+import 'package:rookie_yaml/src/directives/directives.dart';
+
+part 'yaml_failsafe_schema.dart';
+
+base class RookieYamlParser {}
