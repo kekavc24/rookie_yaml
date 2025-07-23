0eb30da5ca583351a8cab56d6dc61f0436304d07
Kelvin Kavisi <68240897+kekavc24@users.noreply.github.com>
2025-07-23 16:50:26 +0100

    refactor: rename `NativeResolverTag`
    * The name was misleading. We will allow nodes to be resolved to a custom object which may (not) be a native `Dart` type

--- lib/src/directives/resolver_tag.dart
+++ lib/src/directives/resolver_tag.dart
@@ -1,9 +1,9 @@
 part of 'directives.dart';
 
 /// A tag that wraps a [ResolvedTag] and defines a mapping function for any
-/// node annotated with wrapped tag.
-final class NativeResolverTag<O> implements ResolvedTag {
-  NativeResolverTag(this._resolvedTag, {required this.resolver});
+/// node annotated with the tag.
+final class TypeResolverTag<T> implements ResolvedTag {
+  TypeResolverTag(this._resolvedTag, {required this.resolver});
 
   /// Underlying [ResolvedTag]
   final ResolvedTag _resolvedTag;
