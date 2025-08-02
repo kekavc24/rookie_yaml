1e035720508961e4b786e5b343817febdf9c5498
Kelvin Kavisi <68240897+kekavc24@users.noreply.github.com>
2025-08-02 14:27:41 +0100

    feat: track buffered scalar content line by line

--- lib/src/parser/scanner/scalar_buffer.dart
+++ lib/src/parser/scanner/scalar_buffer.dart
@@ -5,15 +5,29 @@ final class ScalarBuffer {
   ScalarBuffer({required this.ensureIsSafe, StringBuffer? buffer})
     : _buffer = buffer ?? StringBuffer();
 
+  /// Tracks the lines within a formatted scalar
+  final _contentByLine = <String>[];
+
   /// Ensures the [ReadableChar] is printable before writing
   final bool ensureIsSafe;
 
   /// Actual buffer
   final StringBuffer _buffer;
 
+  /// Saves the current [_buffer] state as a complete line
+  void _flushBuffer() {
+    _contentByLine.add(_buffer.toString());
+    _buffer.clear();
+  }
+
   /// Writes a single [char] to the internal [StringBuffer].
-  void writeChar(ReadableChar char) =>
-      _buffer.write(ensureIsSafe ? char.raw() : char.string);
+  void writeChar(ReadableChar char) {
+    if (char is LineBreak) {
+      return _flushBuffer();
+    }
+
+    _buffer.write(ensureIsSafe ? char.raw() : char.string);
+  }
 
   /// Writes an iterable with an unknown sequence of [ReadableChar].
   ///
@@ -33,9 +47,18 @@ final class ScalarBuffer {
   /// Returns the length of the content
   int get length => _buffer.length;
 
-  /// Returns the contents of the internal [StringBuffer] used to buffer the
-  /// [ReadableChar]
-  String bufferedString() => _buffer.toString();
+  /// Returns the lines in the formatted scalar.
+  ///
+  /// `NOTE:` This method should only be called once the scalar has been
+  /// parsed completely. By default, it assumes the last content buffered
+  /// once called is a separate line.
+  Iterable<String> viewAsLines() {
+    if (_buffer.isNotEmpty) {
+      _flushBuffer();
+    }
+
+    return _contentByLine;
+  }
 
   @override
   String toString() =>
