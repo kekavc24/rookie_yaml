d28cab6870ba2ff1e979a9b2f03217fe69bc6e5e
Kelvin Kavisi <68240897+kekavc24@users.noreply.github.com>
2025-08-03 18:32:40 +0100

    refactor: decouple scalar tags from other tags
    * Add `Uri` tag for Dart
    * Add `isYamlTag` function
    * Minor docs

--- lib/src/schema/yaml_schema.dart
+++ lib/src/schema/yaml_schema.dart
@@ -1,4 +1,5 @@
-part of '../parser/yaml_parser.dart';
+import 'package:rookie_yaml/src/directives/directives.dart';
+import 'package:rookie_yaml/src/schema/nodes/node.dart';
 
 /// Default `YAML` uri prefix
 const _yamlPrefix = 'tag:yaml.org,2002:';
@@ -15,13 +16,13 @@ final _defaultYamlHandle = TagHandle.secondary();
 /// ```
 final yamlGlobalTag = GlobalTag.fromTagUri(_defaultYamlHandle, _yamlPrefix);
 
-/// Generic mapping
+/// Generic [Map]
 final mappingTag = LocalTag.fromTagUri(_defaultYamlHandle, 'map');
 
-/// Generic sequence
+/// Generic [List]
 final sequenceTag = LocalTag.fromTagUri(_defaultYamlHandle, 'seq');
 
-/// Generic string
+/// Generic [String]
 final stringTag = LocalTag.fromTagUri(_defaultYamlHandle, 'str');
 
 //
@@ -29,17 +30,36 @@ final stringTag = LocalTag.fromTagUri(_defaultYamlHandle, 'str');
 // This schema is supported by YAML out of the box.
 //
 
-/// `JSON` null
+/// `JSON` [null]
 final nullTag = LocalTag.fromTagUri(_defaultYamlHandle, 'null');
 
-/// `JSON` boolean
+/// `JSON` [bool]
 final booleanTag = LocalTag.fromTagUri(_defaultYamlHandle, 'bool');
 
-/// `JSON` integer
+/// `JSON` [int]
 final integerTag = LocalTag.fromTagUri(_defaultYamlHandle, 'int');
 
-/// `JSON` floating point
+/// `JSON` [double]
 final floatTag = LocalTag.fromTagUri(_defaultYamlHandle, 'float');
 
-/// YAML scalar tags
-final yamlScalarTags = [stringTag, nullTag, booleanTag, integerTag, floatTag];
+//
+// ** Dart Tags **
+//
+
+/// [Uri] tag
+final uriTag = LocalTag.fromTagUri(_defaultYamlHandle, 'uri');
+
+/// Any [LocalTag] that resolves to a [Scalar]
+final scalarTags = {
+  stringTag,
+  nullTag,
+  booleanTag,
+  integerTag,
+  floatTag,
+  uriTag,
+};
+
+/// Checks if a [tag] is valid tag in the yaml schema. A yaml tag uses the
+/// [TagHandleVariant.secondary] handle
+bool isYamlTag(LocalTag tag) =>
+    tag == mappingTag || tag == sequenceTag || scalarTags.contains(tag);
