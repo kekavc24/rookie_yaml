kekavc24/rookie_yaml

commit c03968493b1892c232649ae6b85d5482e122a936

fix(dumper): force block maps to next line
* Always dump block collections on a new line after encoding if the key is implicit.
* Update test to match output.

lib/src/dumping/dump_mapping.dart
@@ -136,9 +136,10 @@ String _encodeBlockMap<K, V>(
       }
 
       /// Block sequences or block maps whose first key is explicit need to be
-      /// forced to start on a new line with the necessary indent.
+      /// forced to start on a new line with the necessary indent. This includes
+      /// implicit block maps.
       final leading =
-          isCollection && (value.startsWith('- ') || value.startsWith('? '))
+          isCollection && !(value.startsWith('{') || value.startsWith('['))
           ? '\n$mapIndent  '
           : ' ';
 

test/dump_yaml_node_test.dart
@@ -140,7 +140,8 @@ void main() {
 %YAML 1.2
 ---
 normal: 24
-compact: &map !!map {
+compact:
+  &map !!map {
    nested: map
   }
 ...''');

commit f42eeb2f6c7b25bb2c5ad515b15dbd4847d127c4

fix(dumper): avoid forcing implicit flow collections to next line
* Force `_encodeObject` to return the `NodeStyle` information used by the current node.
* Reverts a regression on flow nodes introduced by c039684.

lib/src/dumping/dump_mapping.dart
@@ -28,6 +28,7 @@ String _encodeMapping<K, V>(
     bool hasNext,
     bool keyIsExplicit,
     bool keyHasTrailingLF,
+    bool valueIsFlow,
     bool valueIsCollection,
   )
   onEncodedValue,
@@ -42,7 +43,12 @@ String _encodeMapping<K, V>(
 
   do {
     final MapEntry(:key, :value) = iterator.current;
-    final (:explicitIfKey, isCollection: _, :encoded) = _encodeObject(
+    final (
+      :explicitIfKey,
+      isCollection: _,
+      isFlow: _,
+      :encoded,
+    ) = _encodeObject(
       key,
       indent: keyIndent,
       currentScalarStyle: keyScalarStyle,
@@ -60,7 +66,12 @@ String _encodeMapping<K, V>(
     );
     hasNext = iterator.moveNext();
 
-    final (:isCollection, encoded: object, explicitIfKey: _) = _encodeObject(
+    final (
+      :isCollection,
+      :isFlow,
+      encoded: object,
+      explicitIfKey: _,
+    ) = _encodeObject(
       value,
       currentScalarStyle: valueScalarStyle,
       mapKeyScalarStyle: keyScalarStyle,
@@ -76,6 +87,7 @@ String _encodeMapping<K, V>(
       hasNext,
       explicitIfKey,
       encodedKey.endsWith('\n'),
+      isFlow,
       isCollection,
     );
 
@@ -126,11 +138,11 @@ String _encodeBlockMap<K, V>(
           '${key.startsWith('*') ? ' ' : ''}' // Aliases accept ":"
           ':';
     },
-    onEncodedValue: (value, _, keyIsExplicit, keyHasTrailingLF, isCollection) {
+    onEncodedValue: (value, _, keyIsExplicit, keyHasLF, isFlow, isCollection) {
       final valueTrailer = value.endsWith('\n') ? '' : '\n';
 
       if (keyIsExplicit) {
-        return '${keyHasTrailingLF ? '' : '\n'}'
+        return '${keyHasLF ? '' : '\n'}'
             '$mapIndent: $value'
             '$valueTrailer';
       }
@@ -138,10 +150,7 @@ String _encodeBlockMap<K, V>(
       /// Block sequences or block maps whose first key is explicit need to be
       /// forced to start on a new line with the necessary indent. This includes
       /// implicit block maps.
-      final leading =
-          isCollection && !(value.startsWith('{') || value.startsWith('['))
-          ? '\n$mapIndent  '
-          : ' ';
+      final leading = !isFlow && isCollection ? '\n$mapIndent  ' : ' ';
 
       // Readability's sake
       return '$leading'
@@ -201,7 +210,7 @@ String _encodeFlowMap<K, V>(
           '$key'
           '${key.startsWith('*') ? ' ' : ''}'; // Add space for aliases
     },
-    onEncodedValue: (value, hasNext, _, keyHasTrailingLF, _) {
+    onEncodedValue: (value, hasNext, _, keyHasTrailingLF, _, _) {
       return '${keyHasTrailingLF ? valueIndentation : ''}'
           // Flow maps can ignore the ":" if the value is empty.
           '${value.isEmpty ? '' : ': '}'

lib/src/dumping/dumping.dart
@@ -128,6 +128,7 @@ Iterable<String> _splitBlockString(String blockContent) => splitLazyChecked(
 
 typedef _DumpedObjectInfo = ({
   bool explicitIfKey,
+  bool isFlow,
   bool isCollection,
   String encoded,
 });
@@ -166,6 +167,8 @@ _DumpedObjectInfo _encodeObject<T>(
   String? objectProperties;
   var style = nodeStyle;
 
+  bool isFlow() => style == NodeStyle.flow;
+
   if (unpack != null && object is CompactYamlNode) {
     final (:encodedAlias, :properties, :toEncode, :styleOverride) = unpack(
       object,
@@ -175,6 +178,7 @@ _DumpedObjectInfo _encodeObject<T>(
     if (encodedAlias != null) {
       return (
         encoded: encodedAlias,
+        isFlow: isFlow(),
         isCollection: false,
         explicitIfKey: false,
       );
@@ -197,6 +201,7 @@ _DumpedObjectInfo _encodeObject<T>(
     case Iterable list:
       return (
         explicitIfKey: true,
+        isFlow: isFlow(),
         isCollection: true,
         encoded: _dumpSequence(
           list,
@@ -212,6 +217,7 @@ _DumpedObjectInfo _encodeObject<T>(
     case Map map:
       return (
         explicitIfKey: true,
+        isFlow: isFlow(),
         isCollection: true,
         encoded: _dumpMapping(
           map,
@@ -243,6 +249,7 @@ _DumpedObjectInfo _encodeObject<T>(
 
         return (
           explicitIfKey: explicitIfKey,
+          isFlow: isFlow(),
           isCollection: false,
           encoded: _applyProperties(encodedScalar, objectProperties),
         );

test/dump_yaml_node_test.dart
@@ -140,8 +140,7 @@ void main() {
 %YAML 1.2
 ---
 normal: 24
-compact:
-  &map !!map {
+compact: &map !!map {
    nested: map
   }
 ...''');