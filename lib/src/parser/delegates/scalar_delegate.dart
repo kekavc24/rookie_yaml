74d98be904398c0fa41c636f7cfc338a816f8295
Kelvin Kavisi <68240897+kekavc24@users.noreply.github.com>
2025-11-19 01:38:57 +0000

    fix(parser): default `ScalarDelegate` content to an empty string
    * This allows empty strings annotated with `!!str` to be treated an empty string instead of an `null`. We should infer `null` only when we don't have a tag or have the `nullTag`.

--- lib/src/parser/delegates/scalar_delegate.dart
+++ lib/src/parser/delegates/scalar_delegate.dart
@@ -1,5 +1,18 @@
 part of 'parser_delegate.dart';
 
+/// Creates a `null` wrapped in a [ScalarDelegate].
+ScalarDelegate<T> nullScalarDelegate<T>({
+  required int indentLevel,
+  required int indent,
+  required RuneOffset startOffset,
+  required ScalarFunction<T> resolver,
+}) => ScalarDelegate(
+  indentLevel: indentLevel,
+  indent: indent,
+  start: startOffset,
+  scalarResolver: resolver,
+);
+
 /// A delegate that resolves to a [Scalar].
 final class ScalarDelegate<T> extends ParserDelegate<T> {
   ScalarDelegate({
@@ -8,7 +21,7 @@ final class ScalarDelegate<T> extends ParserDelegate<T> {
     required super.start,
     required this.scalarResolver,
     PreScalar? prescalar,
-  }) : content = prescalar?.content,
+  }) : content = prescalar?.content ?? '',
        wroteLineBreak = prescalar?.wroteLineBreak ?? false,
        scalarStyle = prescalar?.scalarStyle ?? ScalarStyle.plain {
     if (prescalar == null) return;
@@ -19,7 +32,7 @@ final class ScalarDelegate<T> extends ParserDelegate<T> {
   }
 
   /// Parsed content
-  final String? content;
+  final String content;
 
   /// [content] has a line break.
   final bool wroteLineBreak;
@@ -45,38 +58,35 @@ final class ScalarDelegate<T> extends ParserDelegate<T> {
   T _resolver() {
     ScalarValue? value;
 
-    if (content != null) {
-      if (_tag case ContentResolver<dynamic>(
-        :final resolver,
-        :final toYamlSafe,
-        :final acceptNullAsValue,
-      )) {
-        if (resolver(content!) case dynamic resolved
-            when resolved != null || acceptNullAsValue) {
-          value = CustomValue(resolved, toYamlSafe: toYamlSafe);
-        }
+    if (_tag case ContentResolver<dynamic>(
+      :final resolver,
+      :final toYamlSafe,
+      :final acceptNullAsValue,
+    )) {
+      if (resolver(content) case dynamic resolved
+          when resolved != null || acceptNullAsValue) {
+        value = CustomValue(resolved, toYamlSafe: toYamlSafe);
       }
-
-      // Just infer our way if it cannot be resolved or it was never declared
-      value ??= ScalarValue.fromParsedScalar(
-        content!,
-        defaultToString: wroteLineBreak || _tag is NodeResolver,
-        parsedTag: _tag?.suffix,
-        ifParsedTagNull: (inferred) {
-          /// Verbatim tags have no suffix. They are complete and in a
-          /// resolved state as they are.
-          ///
-          /// Type resolver tags are somewhat qualified. They intentionally
-          /// hide the suffix of a resolved tag forcing the scalar to be in
-          /// its natural formatted form after parsing.
-          if (_tag case VerbatimTag _ || TypeResolverTag _) return;
-          _tag = NodeTag(yamlGlobalTag, inferred);
-        },
-      );
     }
 
     return scalarResolver(
-      value ?? NullView(''),
+      // Just infer our way if it cannot be resolved or it was never declared
+      value ??
+          ScalarValue.fromParsedScalar(
+            content,
+            defaultToString: wroteLineBreak || _tag is NodeResolver,
+            parsedTag: _tag?.suffix,
+            ifParsedTagNull: (inferred) {
+              /// Verbatim tags have no suffix. They are complete and in a
+              /// resolved state as they are.
+              ///
+              /// Type resolver tags are somewhat qualified. They intentionally
+              /// hide the suffix of a resolved tag forcing the scalar to be in
+              /// its natural formatted form after parsing.
+              if (_tag case VerbatimTag _ || TypeResolverTag _) return;
+              _tag = _defaultTo(inferred);
+            },
+          ),
       scalarStyle,
       _tag ?? _defaultTo(nullTag),
       _anchor,
