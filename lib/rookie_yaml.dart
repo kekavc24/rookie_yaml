d8f54f5e440974bb97b1414c898cfa60d90eff32
Kelvin Kavisi <68240897+kekavc24@users.noreply.github.com>
2025-12-30 20:43:44 +0000

    chore: expose required libraries

--- lib/rookie_yaml.dart
+++ lib/rookie_yaml.dart
@@ -1,6 +1,9 @@
 library;
 
 export 'src/dumping/dumping.dart';
+export 'src/parser/custom_resolvers.dart';
+export 'src/parser/delegates/object_delegate.dart'
+    show TagInfo, MappingToObject, BytesToScalar, SequenceToObject;
 export 'src/parser/directives/directives.dart'
     hide
         Directives,
@@ -11,14 +14,14 @@ export 'src/parser/directives/directives.dart'
         parseVerbatimTag,
         resolvedTagInfo,
         verbatimStart;
-export 'src/parser/document/yaml_document.dart' hide DocumentParser;
+export 'src/parser/document/node_properties.dart'
+    show ParsedProperty, NodeProperty, isGenericNode;
+export 'src/parser/document/nodes_by_kind/node_kind.dart'
+    show NodeKind, CustomKind, YamlCollectionKind, YamlScalarKind;
+export 'src/parser/document/state/custom_triggers.dart';
+export 'src/parser/document/yaml_document.dart' show YamlDocument;
 export 'src/parser/loaders/loader.dart';
-export 'src/parser/parser_utils.dart'
-    hide
-        seamlessIndentMarker,
-        PreScalar,
-        checkForDocumentMarkers,
-        skipToParsableChar;
+export 'src/parser/parser_utils.dart' show CharWriter;
 export 'src/schema/nodes/yaml_node.dart';
 export 'src/schema/yaml_comment.dart' hide parseComment;
 export 'src/schema/yaml_schema.dart';
